bbbc87b9533201b18f2f21d32e5f85db0a164200
Virantha Mendis <25767841+vmendis@users.noreply.github.com>
2022-09-10 18:06:25 +0530

    Adding notes.

--- 1.deploy-kubernetes-dashboard.sh
+++ 1.deploy-kubernetes-dashboard.sh
@@ -4,6 +4,8 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.6.1/a
 
 # Create a simple user for dashboard
 # Referenece : https://github.com/kubernetes/dashboard/blob/master/docs/user/access-control/creating-sample-user.md
+echo " "
 echo "Creating a service user ..."
+sleep 10
 kubectl apply -f Tools/dashboard-adminuser.yaml
 
