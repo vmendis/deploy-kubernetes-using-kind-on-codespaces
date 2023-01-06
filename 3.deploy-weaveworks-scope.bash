b82ec1f73323ae213d4a267f6264e342860da56d
Virantha Mendis <25767841+vmendis@users.noreply.github.com>
2023-01-06 09:45:10 +0000

    Renamed the script used for deploying weaveworks scope

--- 3.deploy-weaveworks-scope.bash
+++ 3.deploy-weaveworks-scope.bash
@@ -2,7 +2,10 @@
 kubectl apply -f https://github.com/weaveworks/scope/releases/download/v1.13.2/k8s-scope.yaml
 
 echo "*****"
-echo "*****"
+echo " "
+echo "sleeping until pod stabalises"
+echo " "
+sleep 30
 kubectl port-forward -n weave "$(kubectl get -n weave pod --selector=weave-scope-component=app -o jsonpath='{.items..metadata.name}')" 4040
 
 
