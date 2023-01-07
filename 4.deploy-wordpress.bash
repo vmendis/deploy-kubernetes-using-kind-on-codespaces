75f243b4d91b8b45bd29ebeb83af500c6638c2ee
Virantha Mendis <25767841+vmendis@users.noreply.github.com>
2023-01-07 09:28:37 +0000

    Modified WordPress related files

--- 4.deploy-wordpress.bash
+++ 4.deploy-wordpress.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
-kubectl  apply -f Tools/WordPress/create-namespace.yaml
-sleep 30
-kubectl apply -f Tools/WordPress/secret.yaml
-sleep 20
-kubectl apply -f  Tools/WordPress/wordpress.yaml
+kubectl  apply -f Tools/WordPress/0.namespace.yaml
+sleep 10
+kubectl apply -f  Tools/WordPress/1.pv.yaml
+sleep 10
+kubectl apply -f Tools/WordPress/2.mysql.yaml
+sleep 10
+kubectl apply -f Tools/WordPress/3.wordpress.yaml
+sleep 10
 kubectl  --context kind-kind get all,cm,secret,ing -A | grep wordpress | grep pod
